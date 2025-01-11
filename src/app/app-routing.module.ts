010033caa8f69071a32fec20b5148a0a244f1284
Divyansu123 <divyansu.me@gmail.com>
2025-01-12 00:07:23 +0530

    tried again

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginRegisterComponent } from './login-register/login-register.component';
+
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'login-register', component: LoginRegisterComponent },
+
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 ];
